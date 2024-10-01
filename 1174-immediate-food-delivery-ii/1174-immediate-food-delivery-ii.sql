56a21f44db14334534648f7c13545dd0e45c1891
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2024-10-01 13:24:41 +0530

    Time: 644 ms (41.73%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -2,7 +2,7 @@
 
 
 
-SELECT 
+select 
     round(sum(if(order_date = customer_pref_delivery_date, 1, 0)) / count(*) * 100, 2) as immediate_percentage
 from 
     (select customer_id, order_date, customer_pref_delivery_date
